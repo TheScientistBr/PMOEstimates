6e9d58efe580cbca598de1a3a8ee2dd86b283c7a
dbranquinho <delermando@gmail.com>
2017-06-29 15:19:13 -0300

    updating

--- code.R
+++ code.R
@@ -6,19 +6,19 @@ vp3 <- rnorm(40,mean = 10,sd = 5)
 vp <- c(vp1,vp2,vp3)
 remove(vp1,vp2,vp3)
 cr <- vp[1:50] + rnorm(50,mean = 5,sd = 2)
-va <- vp[1:50] + rnorm(50,0,20)
+va <- vp[1:50] + rnorm(50,-3,30)
 
 dias <- seq(1,100,1)
 m <- max(c(vp,va,cr))
 
-df <- data.frame(dias = dias, val = cumsum(vp), class = "vp")
-df <- rbind(df,data.frame(dias = 1:50,val = cumsum(cr), class = "cr"))
-df <- rbind(df,data.frame(dias = 1:50,val = cumsum(va), class = "va"))
+df <- data.frame(dias = dias, valor = cumsum(vp), class = "vp")
+df <- rbind(df,data.frame(dias = 1:50,valor = cumsum(cr), class = "cr"))
+df <- rbind(df,data.frame(dias = 1:50,valor = cumsum(va), class = "va"))
 
-ggplot(data = df, aes(x = dias, y = val, color = class)) + 
-        geom_point(data = subset(df, class %in% c("vp", "cr","va")), aes(group = class)) +
+ggplot(data = df, aes(x = dias, y = valor, color = class)) + 
+        geom_line(data = subset(df, class %in% c("vp", "cr","va")), aes(group = class)) +
         scale_color_manual("Curva S", values = c("vp" = "darkgreen", "cr" = "blue", "va" = "red"))
 
-fit <- lm(df$val[1:50]~v)
+fit <- lm(vp[1:50]~cr[1:50]+va[1:50])
 summary(fit)
-plot(predict(fit))
+plot(predict())
