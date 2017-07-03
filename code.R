5c3506b0e8a4966a881188c29a2a4c51e1173614
dbranquinho <delermando@gmail.com>
2017-07-03 16:36:10 -0300

    update

--- code.R
+++ code.R
@@ -1,24 +1,41 @@
 library("ggplot2")
-{
-        set.seed(1)
-        vp1 <- rnorm(30,mean = 100,sd = 25)
-        vp2 <- rnorm(30,mean = 50,sd = 12)
-        vp3 <- rnorm(40,mean = 10,sd = 5)
-        vp <- c(vp1,vp2,vp3)
-        remove(vp1,vp2,vp3)
-        cr <- vp[1:50] + rnorm(50,mean = 5,sd = 2)
-        va <- vp[1:50] + rnorm(50,-3,30)
-        
-        dias <- seq(1,100,1)
-        m <- max(c(vp,va,cr))
-        
-        df <- data.frame(dias = dias, valor = cumsum(vp), class = "vp")
-        df <- rbind(df,data.frame(dias = 1:50,valor = cumsum(cr), class = "cr"))
-        df <- rbind(df,data.frame(dias = 1:50,valor = cumsum(va), class = "va"))
-        
-        ggplot(data = df, aes(x = dias, y = valor, color = class)) + 
-                geom_line(data = subset(df, class %in% c("vp", "cr","va")), aes(group = class)) +
-                scale_color_manual("Curva S", values = c("vp" = "darkgreen", "cr" = "blue", "va" = "red"))
-        
-        fit <- lm(vp[1:50]~cr[1:50]+va[1:50])
-}
+
+set.seed(1)
+vp1 <- rnorm(30,mean = 100,sd = 25)
+vp2 <- rnorm(30,mean = 50,sd = 12)
+vp3 <- rnorm(40,mean = 10,sd = 5)
+vp <- c(vp1,vp2,vp3)
+remove(vp1,vp2,vp3)
+cr <- vp[1:50] + rnorm(50,mean = 5,sd = 2)
+va <- vp[1:50] + rnorm(50,-3,30)
+
+dias <- seq(1,100,1)
+m <- max(c(vp,va,cr))
+
+df <- data.frame(dias = dias, valor = cumsum(vp), class = "vp")
+df <- rbind(df,data.frame(dias = 1:50,valor = cumsum(cr), class = "cr"))
+df <- rbind(df,data.frame(dias = 1:50,valor = cumsum(va), class = "va"))
+
+fit <- lm(vp[1:50]~c(1:50))
+
+df <- rbind(df,data.frame(dias = 51:100, valor = predict(fit), class = "VP"))
+
+ggplot(data = df, aes(x = dias, y = valor, color = class)) + 
+        geom_line(data = subset(df, class %in% c("vp", "cr","va", "VP")), 
+                  aes(group = class)) +
+        scale_color_manual("Curva S", values = c("vp" = "darkgreen", "cr" = "blue", "va" = "red", "VP" = "green"))
+
+
+
+df2 <- data.frame(dias = dias, vp = vp, cr = cr, va = va)
+df2[51:100,c("va","cr")] <- NA
+require(reshape)
+df2 <- melt(df2 ,  id.vars = 'dias', variable.name = 'series')
+colnames(df2) <- c("dias","variable", "Valor")
+ggplot(data = df2, aes(x = dias, y = Valor, color = variable)) + 
+        geom_point(data = subset(df2, variable %in% c("vp", "cr","va")), 
+                   aes(group = variable)) + 
+        scale_color_manual("Valor", values = c("vp" = "darkgreen", "cr" = "blue", "va" = "red"))
+
+plot(dias[1:50],va)
+
